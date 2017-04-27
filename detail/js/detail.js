9420d21012716fc63c8f6498ee42f25413b96349
GumiDung <dungbui@gumiviet.com>
2017-04-27 17:34:59 +0700

    fix height

--- detail/js/detail.js
+++ detail/js/detail.js
@@ -1,3 +1,18 @@
 /**
  * Created by gumidung on 4/27/17.
  */
+
+$(function () {
+    var wow = new WOW({});
+    wow.init();
+
+    setTimeout(function () {
+        $(".loader-wrap").addClass("active");
+    },500);
+
+    setTimeout(function () {
+        $(".main-content").addClass("active");
+    }, 4000);
+
+    $(".nav-bar").attr("data-wow-delay","5.5s");
+});
